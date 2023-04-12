3df428aa7c2ccb005f1e99833f2ec8ec0aa2b40f
Sivaji <sivaji.ddk@gmail.comgit config --global user.name Sivajigit config user.email sivaji.ddk@gmail.com>
2023-04-12 17:52:39 +0530

    first commit

---
+++ src/main/java/Git_Practice323/Git_Practice323/App.java
@@ -0,0 +1,13 @@
+package Git_Practice323.Git_Practice323;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
